540ffac63576a22a4057ba76c9a008e6970483cf
unknown <dominicgena>
2025-01-05 18:24:48 -0500

    Initial commit

---
+++ src/main/java/com/genadom/genadom00001/MinecraftWebpageViewerClient.java
@@ -0,0 +1,10 @@
+package com.genadom.genadom00001;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MinecraftWebpageViewerClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/genadom/genadom00001/MinecraftWebpageViewerDataGenerator.java
@@ -0,0 +1,11 @@
+package com.genadom.genadom00001;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MinecraftWebpageViewerDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
